b7ae611dec5b961bf25995f51e59cafa24795673
SEMSOFT <erfan_mousavian@yahoo.com>
2021-12-26 10:21:24 +0330

    update agent class

--- Simulator/agents/agent.py
+++ Simulator/agents/agent.py
@@ -7,3 +7,27 @@ class Agent:
 
     def add_usd_to_wallet(self, amount):
         self.wallet.add_usd(amount)
+
+    def buy_basis(self):  # this method can be override by its child
+        pass
+
+    def sell_basis(self):  # this method can be override by its child
+        pass
+
+    def buy_share(self):  # this method can be override by its child
+        pass
+
+    def sell_share(self):  # this method can be override by its child
+        pass
+
+    def buy_bond(self):  # this method can be override by its child
+        pass
+
+    def sell_bond(self):  # this method can be override by its child
+        pass
+
+    def buy_usd(self):  # this method can be override by its child
+        pass
+
+    def sell_usd(self):  # this method can be override by its child
+        pass
