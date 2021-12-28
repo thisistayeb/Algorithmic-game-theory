f675c603dcc983d1f55b3fa109bcc5e787522197
thisistayeb <contact@tayeb.org>
2021-12-28 15:01:13 +0300

    add token_stat

---
+++ Simulator/oracles/token_stat.py
@@ -0,0 +1,29 @@
+from protocol.treasury import treasury, sharetokens, sum_issued_bonds
+
+basis_size_history = []
+basis_price_history = []
+
+def get_basis_history():
+    return basis_size_history, basis_price_history
+
+def get_prior_bond_sum():
+    return sum_issued_bonds
+
+def get_each_basis_reward():
+    basis_price = basis_price_history[-1]
+    if basis_price > 1 :
+        return ((1 - basis_price) * treasury) / sharetokens
+    else:
+        return 0
+
+def get_daily_inflation_rate():
+    daily = basis_price_history[-2] / basis_price_history [-1]
+    contracted = sqrt(daily))
+    return contracted
+
+def get_treasury():
+    return treasury
+
+
+
+    
