833a0ee294689c80a2ed2f985af4f278a1a32086
thisistayeb <contact@tayeb.org>
2021-12-29 17:22:04 +0300

    add random agent

--- Simulator/agents/random_agent.py
+++ Simulator/agents/random_agent.py
@@ -1,10 +1,23 @@
 from agent import Agent
 from wallet.wallet import Wallet
-
+import random
 
 class RandomAgent(Agent):
     def __init__(self, wallet: Wallet):
         super().__init__(wallet)
 
     def action(self):
-        pass
+        """
+        randomly choose to this acrion
+        share <-> usd
+        basis <-> usd
+        basis -> bond
+        """
+        prices = get_token_price()  # (basis, share, bond)
+        if self.wallet.usd > 0:
+            amount = random.uniform(0,self.wallet.usd)
+            self.buy_basis(amount)
+        if self.wallet.basis > 0:
+            amount = random.uniform(0,self.wallet.usd)
+            self.buy_bond(amount)
+            self.sell_basis(amount)
