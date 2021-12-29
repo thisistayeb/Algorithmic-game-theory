1241c723763d57b778fd4d36165339569e05e42f
thisistayeb <contact@tayeb.org>
2021-12-29 20:04:24 +0300

    Remove unnecessary random library

--- Simulator/agents/hugry_trader.py
+++ Simulator/agents/hugry_trader.py
@@ -1,19 +1,19 @@
 from agent import Agent
 from wallet.wallet import Wallet
 
-class NaiveTraderAgent(Agent):
+class HungryTraderAgent(Agent):
     def __init__(self, wallet: Wallet):
         super().__init__(wallet)
 
     def action(self):
         """
-        If Basis price is higher than $1, this trader sells all his Basis tokens
-        If Basis price is less than $1, this trader will change all his USDs to Basis tokens
+        If Basis price is higher than $1.5, this trader sells all his Basis tokens
+        If Basis price is less than $0.5, this trader will change all his USDs to Basis tokens
         """
         prices = get_token_price()  # (basis, share, bond)
-        if price[0] == 1 :
+        if price[0] == 0 :
             pass
-        elif prices[0] > 1:
+        elif prices[0] > 1.5:
             self.sell_basis(self.wallet.basis)
-        elif price[0] < 1:
+        elif price[0] < 0.5:
             self.buy_basis(self.wallet.usd)
