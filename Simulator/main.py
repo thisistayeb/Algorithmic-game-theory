5d25e593f1a8d23e05094d5318614d83525747b1
thisistayeb <contact@tayeb.org>
2022-01-24 14:40:04 +0300

    Add main for Colluges

--- Simulator/main.py
+++ Simulator/main.py
@@ -3,25 +3,25 @@ import protocol.main_protocol as protocol
 import protocol.agent_handler as agent_creator
 import random
 import plotter
-
+from tqdm import tqdm
 ROUNDS = 5
 
 # we make agents of different types here
 # create agents from agent_handler
 agents = []
-num = 10
+num = 200
 num2 = 2 * num
 for i in range(num):
     agents.append(agent_creator.create_ideal_trader(usd=1000, basis=15000 / num2, share=100 / num))
     # agents.append(agent_creator.create_ideal_trader2(usd=1000, basis=10000 / num2, share=100 / num2))
-    agents.append(agent_creator.create_random_agent(usd=0, basis=5000 / num2))
-    # agents.append(agent_creator.create_bond_lover(usd=10, basis=10000 / num2))
-    agents.append(agent_creator.create_bond_lover(usd=10000))
+    agents.append(agent_creator.create_random_agent(usd=200, basis=200))
+    agents.append(agent_creator.create_bond_lover(usd=10, basis=10000 / num2))
+    # agents.append(agent_creator.create_bond_lover(usd=100000))
     # agents.append(agent_creator.create_naive_trader(usd=100, basis=10000 / num2, share=100 / num2))
     # agents.append(agent_creator.create_hungry_trader(usd=100, basis=10000 / num2, share=100 / num2))
 # TODO
 
-for _round in range(ROUNDS):
+for _round in tqdm(range(ROUNDS)):
     print(_round)
     for hour in range(24):
         random.shuffle(agents)
