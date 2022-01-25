37d161f329110acbf6a4ba04697d0b3925efe74b
SEMSOFT <erfan_mousavian@yahoo.com>
2022-01-25 17:36:08 +0330

    plotter updated

--- Simulator/plotter.py
+++ Simulator/plotter.py
@@ -9,12 +9,10 @@ def plot_basis_price():
     basis_history = np.asarray(token_stat.basis_price_history)
     bond_history = np.asarray(token_stat.bond_price_history)
     basis_history = basis_history[2:].reshape(basis_history[2:].shape[0] // period, period)
-    basis_history = np.sum(basis_history, axis=1)
-    basis_history /= period
+    basis_history = np.mean(basis_history, axis=1)
 
     bond_history = bond_history[2:].reshape(bond_history[2:].shape[0] // period, period)
-    bond_history = np.sum(bond_history, axis=1)
-    bond_history /= period
+    bond_history = np.mean(bond_history, axis=1)
     # history = np.minimum(history, 5)
     # plt.plot(np.minimum(bond_history, 3))
     fig, axs = plt.subplots(2, 1)
