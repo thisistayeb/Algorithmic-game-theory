c1cdc0bb383954ecdc48e4a54f37ac47570da9bf
SEMSOFT <erfan_mousavian@yahoo.com>
2022-01-27 17:08:56 +0330

    optimize

--- Simulator/agents/random_agent.py
+++ Simulator/agents/random_agent.py
@@ -2,6 +2,7 @@ from agents.agent import Agent
 from wallet.wallet import Wallet
 from oracles.oracle import get_token_price
 import random
+import numpy as np
 
 
 class RandomAgent(Agent):
@@ -9,20 +10,16 @@ class RandomAgent(Agent):
         super().__init__(wallet)
 
     def action(self):
-        random_amount_basis = random.uniform(0, self.wallet.basis / 10)
-        random_amount_usd = random.uniform(0, self.wallet.usd / 10)
-        random_amount_shares = random.uniform(0, self.wallet.shares / 10)
-        choice = random.choice(
-            ["buy_basis", "sell_basis", "sell_share", "buy_share", "buy_bond"]
-        )
+        r = random.uniform(0, 0.1)
+        choice = random.choice([1, 2, 3, 4, 5])
         prices = get_token_price()  # (basis, share, bond)
-        if choice == "buy_basis":
-            self.buy_basis(random_amount_usd)
-        if choice == "sell_basis":
-            self.sell_basis(random_amount_basis)
-        if choice == "buy_bond" and prices[0] >= prices[2]:
-            self.buy_bond(random_amount_basis)
-        if choice == "buy_share":
-            self.buy_share(random_amount_usd)
-        if choice == "sell_share":
-            self.sell_share(random_amount_shares)
+        if choice == 1:
+            self.buy_basis(self.wallet.usd * r)
+        if choice == 2:
+            self.sell_basis(self.wallet.basis * r)
+        if choice == 3 and prices[0] >= prices[2]:
+            self.buy_bond(self.wallet.basis * r)
+        if choice == 4:
+            self.buy_share(self.wallet.usd * r)
+        if choice == 5:
+            self.sell_share(self.wallet.shares * r)
