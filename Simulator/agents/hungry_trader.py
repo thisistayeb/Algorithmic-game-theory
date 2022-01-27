429a2fd13888b2899ea92f975bcfeb572fffa30a
thisistayeb <contact@tayeb.org>
2022-01-27 11:47:00 +0300

    change the hungry trader to a unbiased version

--- Simulator/agents/hungry_trader.py
+++ Simulator/agents/hungry_trader.py
@@ -9,13 +9,13 @@ class HungryTraderAgent(Agent):
 
     def action(self):
         """
-        If Basis price is higher than $1.5, this trader sells all his Basis tokens
-        If Basis price is less than $0.5, this trader will change all his USDs to Basis tokens
+        This trader sells all of his Basis tokens if the Basis price is double the initial price ($1)
+        When the Basis price is less than half of the initial price, the trader will exchange all his USDs for Basis tokens
         """
         prices = get_token_price()  # (basis, share, bond)
         if prices[0] == 0:
             pass
-        elif prices[0] > 1.5:
+        elif prices[0] > 2:
             self.sell_basis(self.wallet.basis)
         elif prices[0] < 0.5:
             self.buy_basis(self.wallet.usd)
